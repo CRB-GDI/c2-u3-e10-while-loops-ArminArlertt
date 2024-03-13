5bc5a91fdb9449d945b64468a65d09549aa5d0b5
ArminArlertt <145461564+ArminArlertt@users.noreply.github.com>
2024-03-13 10:14:38 -0400

    not finished, very confused

---
+++ exersize 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace exersize_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1 
+
+            Console.WriteLine("*************");
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - New game");
+            Console.WriteLine("2 - Load game");
+            Console.WriteLine("3 - options");
+            Console.WriteLine("4 - quit");
+            Console.WriteLine("*************");
+
+
+        }
+    }
+}
