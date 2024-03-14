78dc7cb7e7907b41d0d31b7282b8a9e56603e119
ArminArlertt <145461564+ArminArlertt@users.noreply.github.com>
2024-03-14 10:14:34 -0400

    trying to find out how to make option 4 quit

--- exersize 10/Program.cs
+++ exersize 10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Design;
 using System.Diagnostics.CodeAnalysis;
+using System.Xml.Serialization;
 
 namespace exersize_10
 {
@@ -8,7 +9,8 @@ namespace exersize_10
         static void Main(string[] args)
         {
             // Problem 1 
-
+            int choice;
+            //Print menue
             Console.WriteLine("*************");
             Console.WriteLine("Menu");
             Console.WriteLine("1 - New game");
@@ -16,8 +18,70 @@ namespace exersize_10
             Console.WriteLine("3 - options");
             Console.WriteLine("4 - quit");
             Console.WriteLine("*************");
+            Console.WriteLine();
 
+            //prompt for and get choice 
+            Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
+            while (choice != 5)
+            {
+                if (choice == 1) 
+                { 
+                Console.WriteLine("Creating new game");
+                    Console.ReadLine();
+                    Console.WriteLine("*************");
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1 - New game");
+                    Console.WriteLine("2 - Load game");
+                    Console.WriteLine("3 - options");
+                    Console.WriteLine("4 - quit");
+                    Console.WriteLine("*************");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+                    choice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
+               
+                
+                if (choice == 2) 
+                {
+                    Console.WriteLine("Load game");
+                    Console.ReadLine();
+                    Console.WriteLine("*************");
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1 - New game");
+                    Console.WriteLine("2 - Load game");
+                    Console.WriteLine("3 - options");
+                    Console.WriteLine("4 - quit");
+                    Console.WriteLine("*************");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+                    choice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
+                if (choice == 3) 
+                Console.WriteLine("Options");
+                Console.ReadLine();
+                Console.WriteLine("*************");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1 - New game");
+                Console.WriteLine("2 - Load game");
+                Console.WriteLine("3 - options");
+                Console.WriteLine("4 - quit");
+                Console.WriteLine("*************");
+                Console.WriteLine();
+
+                Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (choice == 4)
+                    Console.WriteLine("Quit");
+            }
         }
     }
 }
